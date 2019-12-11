df2bcb99e2a5a3595df62bd1241cbcf7839a4298
Robert Griesel <r.griesel@gmail.com>
2019-12-11 23:51:56 +0100

    natbib styles in dialog

--- dialogs/include_bibtex_file/include_bibtex_file.py
+++ dialogs/include_bibtex_file/include_bibtex_file.py
@@ -34,6 +34,8 @@ class IncludeBibTeXFile(Dialog):
         self.settings = settings
         self.styles = ['plain', 'abbrv', 'alpha', 'apalike', 'ieeetr']
         self.style_names = ['Plain', 'Abbrv', 'Alpha', 'Apalike', 'iEEEtr']
+        self.natbib_styles = ['plainnat', 'abbrvnat', 'unsrtnat', 'achemso']
+        self.natbib_style_names = ['Plainnat', 'Abbrvnat', 'Unsrtnat', 'Achemso']
         self.current_values = dict()
 
         self.view = view.IncludeBibTeXFileView(self.main_window)
@@ -51,8 +53,13 @@ class IncludeBibTeXFile(Dialog):
         self.view.style_buttons[self.current_values['style']].set_active(True)
         self.view.style_buttons[self.current_values['style']].toggled()
 
+        self.view.natbib_style_buttons[self.current_values['natbib_style']].set_active(True)
+        self.view.natbib_style_buttons[self.current_values['natbib_style']].toggled()
+
         self.view.create_button.set_sensitive(False)
         self.view.file_chooser_button.unselect_all()
+        self.view.natbib_option.set_active(self.current_values['natbib_toggle'])
+        self.on_natbib_toggled(self.view.natbib_option)
         response = self.view.run()
 
         if response == Gtk.ResponseType.APPLY:
@@ -61,13 +68,25 @@ class IncludeBibTeXFile(Dialog):
         self.view.dialog.hide()
 
     def init_current_values(self):
+        self.current_values['style'] = 'plain'
+        self.current_values['natbib_style'] = 'plainnat'
+        self.current_values['natbib_toggle'] = False
+        self.current_values['filename'] = ''
         presets = self.settings.get_value('app_include_bibtex_file_dialog', 'presets')
         if presets != None:
             presets = pickle.loads(presets)
-            self.current_values['style'] = presets['style']
-        else:
-            self.current_values['style'] = 'plain'
-        self.current_values['filename'] = ''
+            try:
+                style = presets['style']
+                if style in self.styles:
+                    self.current_values['style'] = style
+            except KeyError: pass
+            try:
+                style = presets['natbib_style']
+                if style in self.styles:
+                    self.current_values['natbib_style'] = style
+            except KeyError: pass
+            try: self.current_values['natbib_toggle'] = presets['natbib_toggle']
+            except KeyError: pass
     
     def setup(self):
         file_filter1 = Gtk.FileFilter()
@@ -93,15 +112,57 @@ class IncludeBibTeXFile(Dialog):
 
             image = Gtk.Image.new_from_file(os.path.dirname(__file__) + '/resources/' + style + '.png')
             self.view.preview_stack.add_named(image, style)
+
+        first_button = None
+        for name in self.natbib_style_names:
+            style = name.lower()
+            self.view.natbib_style_buttons[style] = Gtk.RadioButton()
+            if first_button == None: first_button = self.view.natbib_style_buttons[style]
+            box = Gtk.HBox()
+            box.pack_start(Gtk.Label(name), False, False, 0)
+            box.set_margin_right(6)
+            box.set_margin_left(4)
+            self.view.natbib_style_buttons[style].add(box)
+            self.view.natbib_style_buttons[style].set_mode(False)
+            if first_button != None:
+                self.view.natbib_style_buttons[style].join_group(first_button)
+            self.view.natbib_style_switcher.pack_start(self.view.natbib_style_buttons[style], False, False, 0)
+            self.view.natbib_style_buttons[style].connect('toggled', self.on_natbib_style_chosen, style)
+
+            image = Gtk.Image.new_from_file(os.path.dirname(__file__) + '/resources/' + style + '.png')
+            self.view.natbib_preview_stack.add_named(image, style)
+
         self.view.topbox.show_all()
 
         self.view.file_chooser_button.connect('file-set', self.on_file_chosen)
+        self.view.natbib_option.connect('toggled', self.on_natbib_toggled)
 
     def on_file_chosen(self, widget=None):
         self.view.create_button.set_sensitive(True)
         self.current_values['filename'] = self.view.file_chooser_button.get_filename()
 
-    def on_style_chosen(self, togglebutton, style):
+    def on_natbib_toggled(self, togglebutton):
+        self.current_values['natbib_toggle'] = togglebutton.get_active()
+        if togglebutton.get_active():
+            self.view.preview_stack_wrapper.hide()
+            self.view.style_switcher.hide()
+            self.view.natbib_preview_stack_wrapper.show_all()
+            self.view.natbib_style_switcher.show_all()
+        else:
+            self.view.preview_stack_wrapper.show_all()
+            self.view.style_switcher.show_all()
+            self.view.natbib_preview_stack_wrapper.hide()
+            self.view.natbib_style_switcher.hide()
+
+    def on_natbib_style_chosen(self, button, style):
+        if self.natbib_styles.index(style) > self.natbib_styles.index(self.current_values['natbib_style']):
+            self.view.natbib_preview_stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT)
+        else:
+            self.view.natbib_preview_stack.set_transition_type(Gtk.StackTransitionType.SLIDE_RIGHT)
+        self.view.natbib_preview_stack.set_visible_child_name(style)
+        self.current_values['natbib_style'] = style
+
+    def on_style_chosen(self, button, style):
         if self.styles.index(style) > self.styles.index(self.current_values['style']):
             self.view.preview_stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT)
         else:
@@ -117,7 +178,10 @@ class IncludeBibTeXFile(Dialog):
             return ''
 
     def get_style(self):
-        return self.current_values['style']
+        if self.current_values['natbib_toggle']:
+            return self.current_values['natbib_style']
+        else:
+            return self.current_values['style']
 
     def insert_template(self):
         buffer = self.document.get_buffer()
