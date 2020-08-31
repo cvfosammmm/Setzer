b8f1254f165c3d62da594972d4bc0999024b7298
Robert Griesel <r.griesel@gmail.com>
2020-08-31 14:00:14 +0200

    generate dynamic autocomplete proposals on the fly

--- setzer/app/autocomplete_provider/autocomplete_provider.py
+++ setzer/app/autocomplete_provider/autocomplete_provider.py
@@ -32,10 +32,11 @@ class AutocompleteProvider(object):
         self.resources_path = resources_path
 
         self.static_proposals = dict()
+        self.dynamic_word_beginnings = list()
         self.dynamic_proposals = dict()
 
         self.generate_static_proposals()
-        GObject.timeout_add(500, self.generate_dynamic_proposals)
+        self.generate_dynamic_word_beginnings()
 
     def get_items_for_completion_window(self, current_word, last_tabbed_command):
         items = list()
@@ -66,9 +67,89 @@ class AutocompleteProvider(object):
         items = list()
         try: items = self.static_proposals[word[1:].lower()][::-1]
         except KeyError: pass
-        try: dynamic_items = self.dynamic_proposals[word[1:].lower()][::-1]
-        except KeyError: dynamic_items = list()
+        dynamic_items = self.get_dynamic_items(word)[::-1]
         return items + dynamic_items
+
+    def get_dynamic_items(self, word):
+        dynamic_items = list()
+        offset = word.find('{')
+        if offset > 1:
+            word_beginning = word[:offset + 1]
+        else:
+            word_beginning = word
+        if word_beginning in self.dynamic_word_beginnings:
+            dynamic_items += self.get_dynamic_reference_commands(word)
+            dynamic_items += self.get_dynamic_bibliography_commands(word)
+        return dynamic_items
+
+    @timer.timer
+    def get_dynamic_reference_commands(self, word):
+        dynamic_items = list()
+        for document in self.workspace.open_documents:
+            if len(dynamic_items) >= 5: break
+
+            labels_dict = document.parser.get_labels()
+            if 'labels' in labels_dict:
+                labels = labels_dict['labels']
+                if labels != None:
+                    ref_types = list()
+                    ref_types.append(('ref', _('Reference to \'{label}\'')))
+                    ref_types.append(('pageref', _('Reference to page of \'{label}\'')))
+                    ref_types.append(('eqref', _('Reference to \'{label}\', with parantheses')))
+                    for label in iter(labels):
+                        if len(dynamic_items) >= 5: break
+
+                        for ref_type in ref_types:
+                            if len(dynamic_items) >= 5: break
+
+                            command = {'command': ref_type[0] + '{' + label + '}', 'description': ref_type[1].format(label=label)}
+                            if command['command'][:len(word) - 1] == word[1:].lower():
+                                if command['command'] not in [item['command'] for item in dynamic_items]:
+                                    dynamic_items.append(command)
+        return dynamic_items
+
+    @timer.timer
+    def get_dynamic_bibliography_commands(self, word):
+        dynamic_items = list()
+        for document in self.workspace.open_documents:
+            if len(dynamic_items) >= 5: break
+
+            labels_dict = document.parser.get_labels()
+            if 'bibitems' in labels_dict:
+                bibitems = labels_dict['bibitems']
+                if bibitems != None:
+                    ref_types = list()
+                    ref_types.append(('cite', _('Cite \'{bibitem}\'')))
+                    ref_types.append(('citet', _('Cite \'{bibitem}\' (abbreviated)')))
+                    ref_types.append(('citep', _('Cite \'{bibitem}\' (abbreviated with brackets)')))
+                    ref_types.append(('citet*', _('Cite \'{bibitem}\' (detailed)')))
+                    ref_types.append(('citep*', _('Cite \'{bibitem}\' (detailed with brackets)')))
+                    ref_types.append(('citealt', _('Cite \'{bibitem}\' (alternative style 1)')))
+                    ref_types.append(('citealp', _('Cite \'{bibitem}\' (alternative style 2)')))
+                    ref_types.append(('citeauthor', _('Cite \'{bibitem}\' (author)')))
+                    ref_types.append(('citeauthor*', _('Cite \'{bibitem}\' (author detailed)')))
+                    ref_types.append(('citeyear', _('Cite \'{bibitem}\' (year)')))
+                    ref_types.append(('citeyearpar', _('Cite \'{bibitem}\' (year with brackets)')))
+                    for bibitem in iter(bibitems):
+                        if len(dynamic_items) >= 5: break
+
+                        for ref_type in ref_types:
+                            if len(dynamic_items) >= 5: break
+
+                            command = {'command': ref_type[0] + '{' + bibitem + '}', 'description': ref_type[1].format(bibitem=bibitem)}
+                            if command['command'][:len(word) - 1] == word[1:].lower():
+                                if command['command'] not in [item['command'] for item in dynamic_items]:
+                                    dynamic_items.append(command)
+        return dynamic_items
+
+    def get_commands(self):
+        commands = dict()
+        tree = ET.parse(os.path.join(self.resources_path, 'latexdb', 'commands', 'general.xml'))
+        root = tree.getroot()
+        for child in root:
+            attrib = child.attrib
+            commands[attrib['name']] = {'command': attrib['text'], 'description': attrib['description']}
+        return commands
 
     def generate_static_proposals(self):
         commands = self.get_commands()
@@ -82,66 +163,11 @@ class AutocompleteProvider(object):
                 except KeyError:
                     self.static_proposals[command['command'][0:i].lower()] = [command]
 
-    #@timer.timer
-    def generate_dynamic_proposals(self):
-        self.dynamic_proposals = dict()
-        for document in self.workspace.open_documents:
-            labels_dict = document.parser.get_labels()
-            try:
-                self.add_dynamic_reference_commands(labels_dict['labels'])
-            except KeyError: pass
-            self.add_dynamic_bibliography_commands(labels_dict['bibitems'])
-        return True
+    def generate_dynamic_word_beginnings(self):
+        commands = ('ref{', 'pageref{', 'eqref{', 'cite{', 'citet{', 'citep{', 'citet*{', 'citep*{', 'citealt{', 'citealp{', 'citeauthor{', 'citeauthor*{', 'citeyear{', 'citeyearpar{')
 
-    #@timer.timer
-    def add_dynamic_reference_commands(self, labels):
-        if labels != None:
-            ref_types = list()
-            ref_types.append(('ref', _('Reference to \'{label}\'')))
-            ref_types.append(('pageref', _('Reference to page of \'{label}\'')))
-            ref_types.append(('eqref', _('Reference to \'{label}\', with parantheses')))
-            for label in iter(labels):
-                for ref_type in ref_types:
-                    command = {'command': ref_type[0] + '{' + label + '}', 'description': ref_type[1].format(label=label)}
-                    for i in range(1, len(command['command']) + 1):
-                        try:
-                            if len(self.dynamic_proposals[command['command'][0:i].lower()]) < 5:
-                                self.dynamic_proposals[command['command'][0:i].lower()].append(command)
-                        except KeyError:
-                            self.dynamic_proposals[command['command'][0:i].lower()] = [command]
-
-    #@timer.timer
-    def add_dynamic_bibliography_commands(self, bibitems):
-        if bibitems != None:
-            ref_types = list()
-            ref_types.append(('cite', _('Cite \'{bibitem}\'')))
-            ref_types.append(('citet', _('Cite \'{bibitem}\' (abbreviated)')))
-            ref_types.append(('citep', _('Cite \'{bibitem}\' (abbreviated with brackets)')))
-            ref_types.append(('citet*', _('Cite \'{bibitem}\' (detailed)')))
-            ref_types.append(('citep*', _('Cite \'{bibitem}\' (detailed with brackets)')))
-            ref_types.append(('citealt', _('Cite \'{bibitem}\' (alternative style 1)')))
-            ref_types.append(('citealp', _('Cite \'{bibitem}\' (alternative style 2)')))
-            ref_types.append(('citeauthor', _('Cite \'{bibitem}\' (author)')))
-            ref_types.append(('citeauthor*', _('Cite \'{bibitem}\' (author detailed)')))
-            ref_types.append(('citeyear', _('Cite \'{bibitem}\' (year)')))
-            ref_types.append(('citeyearpar', _('Cite \'{bibitem}\' (year with brackets)')))
-            for ref_type in ref_types:
-                for bibitem in iter(bibitems):
-                    command = {'command': ref_type[0] + '{' + bibitem + '}', 'description': ref_type[1].format(bibitem=bibitem)}
-                    for i in range(1, len(command['command']) + 1):
-                        try:
-                            if len(self.dynamic_proposals[command['command'][0:i].lower()]) < 5:
-                                self.dynamic_proposals[command['command'][0:i].lower()].append(command)
-                        except KeyError:
-                            self.dynamic_proposals[command['command'][0:i].lower()] = [command]
-
-    def get_commands(self):
-        commands = dict()
-        tree = ET.parse(os.path.join(self.resources_path, 'latexdb', 'commands', 'general.xml'))
-        root = tree.getroot()
-        for child in root:
-            attrib = child.attrib
-            commands[attrib['name']] = {'command': attrib['text'], 'description': attrib['description']}
-        return commands
-
+        self.dynamic_word_beginnings = list()
+        for command in commands:
+            for i in range(1, len(command) + 1):
+                self.dynamic_word_beginnings.append('\\' + command[:i])
 
