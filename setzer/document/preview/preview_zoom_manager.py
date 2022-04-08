8a8ad25ac127ef41b5e442f0c77c25af12d7e9d2
Robert Griesel <r.griesel@gmail.com>
2022-04-08 13:55:57 +0200

    pdfminer is no longer needed

--- setzer/document/preview/preview_zoom_manager.py
+++ setzer/document/preview/preview_zoom_manager.py
@@ -15,11 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>
 
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdftypes import PDFObjRef
-import _thread as thread, queue
-import math
-
 from setzer.helpers.observable import Observable
 
 
