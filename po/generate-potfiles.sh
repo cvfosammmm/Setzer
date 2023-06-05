45332500112080638644c902f76a7bfc92ee283a
oscfdezdz <42654671+oscfdezdz@users.noreply.github.com>
2023-06-05 20:35:11 +0200

    Use metainfo instead of appdata

--- po/generate-potfiles.sh
+++ po/generate-potfiles.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$(realpath "$0")")/.."
 echo "# generated on $(date -u)" > ./po/POTFILES
 
 echo "" >> ./po/POTFILES
-echo "./data/org.cvfosammmm.Setzer.appdata.xml.in" >> ./po/POTFILES
+echo "./data/org.cvfosammmm.Setzer.metainfo.xml.in" >> ./po/POTFILES
 
 echo "" >> ./po/POTFILES
 find ./setzer -name '*.py' | sort >> ./po/POTFILES
