eba27a78aa2a4d27d07530f9c54973b5763ad315
Robert Griesel <r.griesel@gmail.com>
2023-09-20 14:37:51 +0200

    preview: also recolor images

--- setzer/document/preview/preview_presenter.py
+++ setzer/document/preview/preview_presenter.py
@@ -23,6 +23,8 @@ import cairo
 import os.path
 import math
 import time
+import numpy as np
+from PIL import Image, ImageFilter
 
 from setzer.app.color_manager import ColorManager
 from setzer.helpers.timer import timer
@@ -145,8 +147,7 @@ class PreviewPresenter(object):
 
         colored_surface = cairo.ImageSurface(cairo.Format.ARGB32, int(self.preview.layout.page_width / factor), int(self.preview.layout.page_height / factor))
         temp_ctx = cairo.Context(colored_surface)
-        temp_ctx.set_source_rgba(1, 1, 1, 0)
-        temp_ctx.set_operator(cairo.Operator.OVER)
+        temp_ctx.set_source_rgba(1, 1, 1, 1)
         temp_ctx.rectangle(0, 0, self.preview.layout.page_width / factor, self.preview.layout.page_height / factor)
         temp_ctx.fill()
         temp_ctx.set_source_surface(surface, 0, 0)
@@ -154,6 +155,17 @@ class PreviewPresenter(object):
         temp_ctx.fill()
 
         if self.preview.recolor_pdf:
+            pil_img = Image.frombuffer("RGBA", (colored_surface.get_width(), colored_surface.get_height()), colored_surface.get_data(), "raw", "RGBA", 0, 1)
+
+            img_data = np.array(pil_img, dtype=np.ubyte)
+            alpha = 255 - 0.3 * img_data[..., 0] - 0.6 * img_data[..., 1] - 0.1 * img_data[..., 2]
+            img_data[:,:,-1] = alpha
+            pil_img = Image.fromarray(np.ubyte(img_data))
+
+            im_bytes = bytearray(pil_img.tobytes('raw', 'BGRa'))
+            colored_surface = cairo.ImageSurface.create_for_data(im_bytes, cairo.FORMAT_ARGB32, int(self.preview.layout.page_width / factor), int(self.preview.layout.page_height / factor))
+            temp_ctx = cairo.Context(colored_surface)
+
             Gdk.cairo_set_source_rgba(temp_ctx, ColorManager.get_ui_color('view_fg_color'))
             temp_ctx.set_operator(cairo.Operator.IN)
             temp_ctx.rectangle(0, 0, self.preview.layout.page_width / factor, self.preview.layout.page_height / factor)
