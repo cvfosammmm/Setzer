cff6b59540ddc151ee87682cdb35e5246991b7f6
Robert Griesel <r.griesel@gmail.com>
2023-12-06 10:37:45 +0100

    use GtkFileDialog for save document and save session dialogs

--- setzer/dialogs/save_document/save_document.py
+++ setzer/dialogs/save_document/save_document.py
@@ -33,10 +33,17 @@ class SaveDocumentDialog(object):
     def run(self, document):
         self.document = document
         self.setup()
+        self.view.save(self.main_window, None, self.dialog_process_response)
+
+    def setup(self):
+        self.view = Gtk.FileDialog()
+        self.view.set_modal(True)
+        self.view.set_title(_('Save document'))
+
         pathname = self.document.get_filename()
         if pathname != None:
-            self.view.set_current_name(os.path.basename(pathname))
-            self.view.set_current_folder(Gio.File.new_for_path(self.document.get_dirname()))
+            self.view.set_initial_name(os.path.basename(pathname))
+            self.view.set_initial_folder(Gio.File.new_for_path(self.document.get_dirname()))
         else:
             if self.document.get_document_type() == 'latex':
                 ending = '.tex'
@@ -44,32 +51,17 @@ class SaveDocumentDialog(object):
                 ending = '.bib'
             else:
                 ending = ''
-            self.view.set_current_name(ending)
-        self.view.present()
-        self.signal_connection_id = self.view.connect('response', self.process_response)
+            self.view.set_initial_name(ending)
 
-    def process_response(self, view, response_id):
-        if response_id == Gtk.ResponseType.OK:
-            filename = self.view.get_file().get_path()
-            self.document.set_filename(filename)
-            self.document.save_to_disk()
-            self.workspace.update_recently_opened_document(filename)
-        self.close()
-
-    def close(self):
-        self.view.close()
-        self.view.disconnect(self.signal_connection_id)
-        del(self.view)
-
-    def setup(self):
-        self.view = Gtk.FileChooserDialog()
-        self.view.set_transient_for(self.main_window)
-        self.view.set_modal(True)
-        self.view.set_action(Gtk.FileChooserAction.SAVE)
-        self.view.add_button(_('_Cancel'), Gtk.ResponseType.CANCEL)
-        button_open = Gtk.Button.new_with_mnemonic(_('_Save'))
-        button_open.get_style_context().add_class('suggested-action')
-        self.view.add_action_widget(button_open, Gtk.ResponseType.OK)
-        self.view.set_title(_('Save document'))
+    def dialog_process_response(self, dialog, result):
+        try:
+            file = dialog.save_finish(result)
+        except Exception: pass
+        else:
+            if file != None:
+                filename = file.get_path()
+                self.document.set_filename(filename)
+                self.document.save_to_disk()
+                self.workspace.update_recently_opened_document(filename)
 
 
--- setzer/dialogs/save_session/save_session.py
+++ setzer/dialogs/save_session/save_session.py
@@ -31,42 +31,33 @@ class SaveSessionDialog(object):
 
     def run(self):
         self.setup()
+        self.view.save(self.main_window, None, self.dialog_process_response)
+
+    def setup(self):
+        self.view = Gtk.FileDialog()
+        self.view.set_modal(True)
+        self.view.set_title(_('Save Session'))
 
         if self.workspace.session_file_opened != None:
-            self.view.set_current_folder(Gio.File.new_for_path(os.path.dirname(self.workspace.session_file_opened)))
-            self.view.set_current_name(os.path.basename(self.workspace.session_file_opened))
+            self.view.set_initial_folder(Gio.File.new_for_path(os.path.dirname(self.workspace.session_file_opened)))
+            self.view.set_initial_name(os.path.basename(self.workspace.session_file_opened))
         else:
             document = self.workspace.get_root_or_active_latex_document()
             if document != None:
                 pathname = document.get_filename()
                 if pathname != None:
-                    self.view.set_current_folder(Gio.File.new_for_path(os.path.dirname(pathname)))
-                self.view.set_current_name('.stzs')
+                    self.view.set_initial_folder(Gio.File.new_for_path(os.path.dirname(pathname)))
+                self.view.set_initial_name('.stzs')
             else:
-                self.view.set_current_name('.stzs')
-        self.view.present()
-        self.signal_connection_id = self.view.connect('response', self.process_response)
+                self.view.set_initial_name('.stzs')
 
-    def process_response(self, view, response_id):
-        if response_id == Gtk.ResponseType.OK:
-            filename = self.view.get_file().get_path()
-            self.workspace.save_session(filename)
-        self.close()
-
-    def close(self):
-        self.view.close()
-        self.view.disconnect(self.signal_connection_id)
-        del(self.view)
-
-    def setup(self):
-        self.view = Gtk.FileChooserDialog()
-        self.view.set_transient_for(self.main_window)
-        self.view.set_modal(True)
-        self.view.set_action(Gtk.FileChooserAction.SAVE)
-        self.view.add_button(_('_Cancel'), Gtk.ResponseType.CANCEL)
-        button_open = Gtk.Button.new_with_mnemonic(_('_Save'))
-        button_open.get_style_context().add_class('suggested-action')
-        self.view.add_action_widget(button_open, Gtk.ResponseType.OK)
-        self.view.set_title(_('Save Session'))
+    def dialog_process_response(self, dialog, result):
+        try:
+            file = dialog.save_finish(result)
+        except Exception: pass
+        else:
+            if file != None:
+                filename = file.get_path()
+                self.workspace.save_session(filename)
 
 
